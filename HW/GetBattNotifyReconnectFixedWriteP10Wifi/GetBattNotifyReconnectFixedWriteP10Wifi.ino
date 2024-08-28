4fb72d17bb8eebdfb42e98091fe1ec9bcd5072b6
latiful_hayat <stufi1983@gmail.com>
2024-08-28 14:52:41 +0700

    json

--- HW/GetBattNotifyReconnectFixedWriteP10Wifi/GetBattNotifyReconnectFixedWriteP10Wifi.ino
+++ HW/GetBattNotifyReconnectFixedWriteP10Wifi/GetBattNotifyReconnectFixedWriteP10Wifi.ino
@@ -1,62 +1,12 @@
-#include <WiFi.h>
-#include <WiFiClientSecure.h>
-#include <ArduinoJson.h>
-
-const char* ssid = "B0401";
-const char* password = "44444444";
-
-const char* server = "stufimedia.com";  // Server URL
-const char* url = "/posttest.php";  // Server URL
-
-WiFiClientSecure client;
-
-//Make sure you get the root's certificate (save file to .crt)
-//https://randomnerdtutorials.com/esp32-https-requests
-
-/// Root certificate (replace with the provided certificate)
-const char* root_ca = \
-"-----BEGIN CERTIFICATE-----\n"
-"MIIF3jCCA8agAwIBAgIQAf1tMPyjylGoG7xkDjUDLTANBgkqhkiG9w0BAQwFADCB\n"
-"iDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCk5ldyBKZXJzZXkxFDASBgNVBAcTC0pl\n"
-"cnNleSBDaXR5MR4wHAYDVQQKExVUaGUgVVNFUlRSVVNUIE5ldHdvcmsxLjAsBgNV\n"
-"BAMTJVVTRVJUcnVzdCBSU0EgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkwHhcNMTAw\n"
-"MjAxMDAwMDAwWhcNMzgwMTE4MjM1OTU5WjCBiDELMAkGA1UEBhMCVVMxEzARBgNV\n"
-"BAgTCk5ldyBKZXJzZXkxFDASBgNVBAcTC0plcnNleSBDaXR5MR4wHAYDVQQKExVU\n"
-"aGUgVVNFUlRSVVNUIE5ldHdvcmsxLjAsBgNVBAMTJVVTRVJUcnVzdCBSU0EgQ2Vy\n"
-"dGlmaWNhdGlvbiBBdXRob3JpdHkwggIiMA0GCSqGSIb3DQEBAQUAA4ICDwAwggIK\n"
-"AoICAQCAEmUXNg7D2wiz0KxXDXbtzSfTTK1Qg2HiqiBNCS1kCdzOiZ/MPans9s/B\n"
-"3PHTsdZ7NygRK0faOca8Ohm0X6a9fZ2jY0K2dvKpOyuR+OJv0OwWIJAJPuLodMkY\n"
-"tJHUYmTbf6MG8YgYapAiPLz+E/CHFHv25B+O1ORRxhFnRghRy4YUVD+8M/5+bJz/\n"
-"Fp0YvVGONaanZshyZ9shZrHUm3gDwFA66Mzw3LyeTP6vBZY1H1dat//O+T23LLb2\n"
-"VN3I5xI6Ta5MirdcmrS3ID3KfyI0rn47aGYBROcBTkZTmzNg95S+UzeQc0PzMsNT\n"
-"79uq/nROacdrjGCT3sTHDN/hMq7MkztReJVni+49Vv4M0GkPGw/zJSZrM233bkf6\n"
-"c0Plfg6lZrEpfDKEY1WJxA3Bk1QwGROs0303p+tdOmw1XNtB1xLaqUkL39iAigmT\n"
-"Yo61Zs8liM2EuLE/pDkP2QKe6xJMlXzzawWpXhaDzLhn4ugTncxbgtNMs+1b/97l\n"
-"c6wjOy0AvzVVdAlJ2ElYGn+SNuZRkg7zJn0cTRe8yexDJtC/QV9AqURE9JnnV4ee\n"
-"UB9XVKg+/XRjL7FQZQnmWEIuQxpMtPAlR1n6BB6T1CZGSlCBst6+eLf8ZxXhyVeE\n"
-"Hg9j1uliutZfVS7qXMYoCAQlObgOK6nyTJccBz8NUvXt7y+CDwIDAQABo0IwQDAd\n"
-"BgNVHQ4EFgQUU3m/WqorSs9UgOHYm8Cd8rIDZsswDgYDVR0PAQH/BAQDAgEGMA8G\n"
-"A1UdEwEB/wQFMAMBAf8wDQYJKoZIhvcNAQEMBQADggIBAFzUfA3P9wF9QZllDHPF\n"
-"Up/L+M+ZBn8b2kMVn54CVVeWFPFSPCeHlCjtHzoBN6J2/FNQwISbxmtOuowhT6KO\n"
-"VWKR82kV2LyI48SqC/3vqOlLVSoGIG1VeCkZ7l8wXEskEVX/JJpuXior7gtNn3/3\n"
-"ATiUFJVDBwn7YKnuHKsSjKCaXqeYalltiz8I+8jRRa8YFWSQEg9zKC7F4iRO/Fjs\n"
-"8PRF/iKz6y+O0tlFYQXBl2+odnKPi4w2r78NBc5xjeambx9spnFixdjQg3IM8WcR\n"
-"iQycE0xyNN+81XHfqnHd4blsjDwSXWXavVcStkNr/+XeTWYRUc+ZruwXtuhxkYze\n"
-"Sf7dNXGiFSeUHM9h4ya7b6NnJSFd5t0dCy5oGzuCr+yDZ4XUmFF0sbmZgIn/f3gZ\n"
-"XHlKYC6SQK5MNyosycdiyA5d9zZbyuAlJQG03RoHnHcAP9Dc1ew91Pq7P8yF1m9/\n"
-"qS3fuQL39ZeatTXaw2ewh0qpKJ4jjv9cJ2vhsE/zB+4ALtRZh8tSQZXq9EfX7mRB\n"
-"VXyNWQKV3WKdwrnuWih0hKWbt5DHDAff9Yk2dDLWKMGwsAvgnEzDHNb842m1R0aB\n"
-"L6KCq9NjRHDEjf8tM7qtj3u1cIiuPhnPQCjY/MiQu12ZIvVS5ljFH4gxQ+6IHdfG\n"
-"jjxDah2nGN59PRbxYvnKkKj9\n"
-"-----END CERTIFICATE-----\n";
-
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEScan.h>
 #include <BLEClient.h>
 
 #include <esp_task_wdt.h>
-
+#include <ArduinoJson.h>
+#define RXD1 12
+#define TXD1 13
 
 #define RXD2 16
 #define TXD2 17
@@ -389,7 +339,7 @@ void displayP10() {
     else {
       if (curHeartRate > 0 && curSPO2 > 0) {
         if (sehat) stat = " Sehat Bugar ";
-        else stat =       "Kurang  Sehat";
+        else stat = "Kurang  Sehat";
       } else {
         stat = "Pengukuran...";
       }
@@ -402,8 +352,10 @@ void displayP10() {
 
 void setup() {
   Serial.begin(115200);
-
+  Serial1.begin(115200, SERIAL_8N1, RXD1, TXD1);
   Serial2.begin(1200, SERIAL_8N1, RXD2, TXD2);
+
+  Serial1.println("Thisis ates");
 
   BLEDevice::init("");
   scanBLEDev();
@@ -411,77 +363,7 @@ void setup() {
     connectToDevice();
     doReconnect = false;
   }
-esp_pause_bluetooth();
-  Serial.print("Attempting to connect to SSID: ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
 
-  // Attempt to connect to WiFi network:
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(1000); // Wait 1 second before retrying
-  }
-
-  Serial.println("\nConnected to WiFi!");
-
-  //client.setInsecure(); // For now, ignore SSL validation (insecure)
-client.setCACert(root_ca);  // Set the root certificate for SSL
-
-  Serial.println("Starting connection to server...");
-  if (!client.connect(server, 443)) {
-    Serial.println("Connection failed!");
-    return;
-  } else {
-    Serial.println("Connected to server!");
-
-    // Create JSON data to send in POST request
-    DynamicJsonDocument doc(1024);
-
-    // Add values to JSON document
-    doc["key"] = "kunci";
-    doc["sensor"] = "gps";
-    doc["time"] = 1351824120;
-
-    // Add array data
-    JsonArray data = doc.createNestedArray("data");
-    data.add(48.756080);
-    data.add(2.302038);
-
-    // Convert JSON document to string
-    String postData;
-    serializeJson(doc, postData);
-
-    // Start a POST request
-    client.println("POST "+String(url)+" HTTP/1.1");
-    client.println("Host: " + String(server));
-    //client.println("Content-Type: application/json"); //Doesn't work with $_POST, but work with: $data = json_decode(file_get_contents('php://input'), true);
-    client.println("Content-Type: application/x-www-form-urlencoded");
-    client.println("Connection: close");
-    client.println("Content-Length: " + String(postData.length()));
-    client.println();
-    client.println(postData);
-
-    // Wait for the response and print the headers
-    while (client.connected()) {
-      String line = client.readStringUntil('\n');
-      if (line == "\r") {
-        Serial.println("Headers received.");
-        break;
-      }
-    }
-
-    // Read and print the response
-    String response;
-    while (client.available()) {
-      response = client.readString();
-      Serial.println("Response: " + response);
-    }
-
-    // Close the connection
-    client.stop();
-  }
-  esp_unpause_bluetooth();
-  /*
   // Initialize the watchdog timer
   // Create the watchdog configuration
   esp_task_wdt_config_t wdt_config = {
@@ -492,12 +374,37 @@ client.setCACert(root_ca);  // Set the root certificate for SSL
 
   // Initialize the watchdog timer with the configuration
   esp_task_wdt_init(&wdt_config);
-  esp_task_wdt_add(NULL);  // Add current thread to the watchdog timer*/
+  esp_task_wdt_add(NULL);  // Add current thread to the watchdog timer
+}
+
+
+void sendJsonSerial() {
+  // Allocate the JSON document
+  JsonDocument doc;
+
+  // Add values in the document
+  doc["sensor"] = "gps";
+  doc["time"] = 1351824120;
+
+  // Add an array
+  JsonArray data = doc["data"].to<JsonArray>();
+  data.add(48.756080);
+  data.add(2.302038);
+
+  // Generate the minified JSON and send it to the Serial port
+  serializeJson(doc, Serial1);
 }
 
 unsigned long previousOLMillis = 0;
 
 void loop() {
+  byte RxData;
+  // put your main code here, to run repeatedly:
+  if (Serial1.available() > 0) {
+    RxData = Serial1.read();
+    Serial.write(RxData);
+  }
+
   unsigned long currentOLMillis = millis();
 
   // Check if 3 second has passed
@@ -540,6 +447,7 @@ void loop() {
     previousMillis = currentMillis;
     MICounter++;
     Serial.println("Loop");
+    sendJsonSerial();
     if (isConnected) {
       readBatteryLevel();
       if (curBatteryLevel > 5) {
@@ -592,7 +500,7 @@ void loop() {
   }
 
   // Reset the watchdog timer
- // esp_task_wdt_reset();
+  esp_task_wdt_reset();
 }
 
 void readBatteryLevel() {
